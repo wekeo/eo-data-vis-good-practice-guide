4bfa914eed95f8054bcb1402a18f655a88dc2363
wekeo <Hayley.EversKing@eumetsat.int>
2023-08-08 11:15:16 +0100

    quick pre-commit test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ org = 'EUMETSAT'
 organization = f'{org} on behalf of the Copernicus Programme'
 author = f'{org} and contributors'
 copyright = f'CC BY 4.0 (2023)'
-version = '0.1.2'
+version = '0.1.3'
 release = version
 
 # -- General configuration
